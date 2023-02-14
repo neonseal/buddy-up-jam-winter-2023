e69f461a7dd3a6b916fb9129af5b5e1ec0fc4575
laumi73 <laumichael73@gmail.com>
2023-02-14 01:31:30 -0500

    Setup very basic logic of adding nested components to parent gameObject for main scene

--- Assets/Scripts/Plushie.cs
+++ Assets/Scripts/Plushie.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Plushie : MonoBehaviour
 {
+    private GameObject plushieDamage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.AddPlushieDamageToScene();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void AddPlushieDamageToScene()
+    {
+        this.plushieDamage = new GameObject();
+        this.plushieDamage.name = "PlushieDamage";
+        this.plushieDamage.AddComponent<PlushieDamage>();
+        this.plushieDamage.transform.SetParent(this.transform);
     }
 }
--- Assets/Scripts/RepairWorkspace.cs
+++ Assets/Scripts/RepairWorkspace.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class RepairWorkspace : MonoBehaviour
 {
+    private GameObject plushie;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.AddPlushieToScene();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void AddPlushieToScene()
+    {
+        this.plushie = new GameObject();
+        this.plushie.name = "Plushie";
+        this.plushie.AddComponent<Plushie>();
+        this.plushie.transform.SetParent(this.transform);
     }
 }
