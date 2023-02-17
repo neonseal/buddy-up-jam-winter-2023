9118a71d02803f79ee91b955b56ae684764fec92
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-02-17 10:14:49 -0500

    Tweak client card and add text

--- Assets/Scripts/ClientCardController.cs
+++ Assets/Scripts/ClientCardController.cs
@@ -6,24 +6,35 @@ using DG.Tweening;
 
 public class ClientCardController : MonoBehaviour, IPointerDownHandler {
     //[SerializeField] private Transform card;
+    [SerializeField] private int xPos = 0;
+    [SerializeField] private int yPos = 0;
+
     [SerializeField] private float cycleLength = 2f;
     [SerializeField] private Ease easeType;
-    [SerializeField] private float targetScale;
 
     private Vector3 startingPosition;
+    public bool moving;
 
     private void Awake() {
         DOTween.Init();
-        startingPosition = this.gameObject.transform.position;
-        easeType = Ease.InCirc;
+        startingPosition = this.gameObject.transform.localPosition;
+    }
+
+    private void Update() {
+        if (this.gameObject.transform.localPosition == startingPosition || this.gameObject.transform.localPosition == new Vector3(xPos, yPos)) {
+            moving = false;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData) {
-        if (this.gameObject.transform.position == startingPosition) {
-            this.gameObject.transform.DOMove(new Vector3(-7, 3), cycleLength).SetEase(easeType);
-            //this.gameObject.transform.DOScale(targetScale, cycleLength);
-        } else {
-            this.gameObject.transform.DOMove(startingPosition, cycleLength).SetEase(easeType);
+        if (!moving) {
+            if (this.gameObject.transform.localPosition == startingPosition) {
+                moving = true;
+                this.gameObject.transform.DOLocalMove(new Vector3(xPos, yPos), cycleLength).SetEase(Ease.InCubic);
+            } else {
+                moving = true;
+                this.gameObject.transform.DOLocalMove(startingPosition, cycleLength).SetEase(Ease.InCubic);
+            }
         }
     }
 }
