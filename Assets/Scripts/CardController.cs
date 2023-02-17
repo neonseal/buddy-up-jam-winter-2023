4adbac6289dafbece73ba910d3420794c128344d
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-02-17 11:14:35 -0500

    Layout checklist UI element

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CardController : MonoBehaviour
 {
     public CardScriptableObject cardDescriptionData;
-    private TextMesh[] descriptionZones;
+    private TMP_Text[] descriptionZones;
 
     private void Awake() {
-        descriptionZones = GetComponentsInChildren<TextMesh>();
+        descriptionZones = GetComponentsInChildren<TMP_Text>();
+        descriptionZones[0].text = cardDescriptionData.description_part1;
+        descriptionZones[1].text = cardDescriptionData.description_part2;
     }
 
-    
+
 }
