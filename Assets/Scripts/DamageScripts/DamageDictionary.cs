6ddc3e09da636fb370b2b09b087734baf4fdc585
laumi73 <laumichael73@gmail.com>
2023-02-17 02:39:55 -0500

    Revert "Minor renaming"
    This reverts commit a08a7c04a4ddd568b0579cbed5f2ed9072841b6d.

--- Assets/Scripts/DamageScripts/DamageDictionary.cs
+++ Assets/Scripts/DamageScripts/DamageDictionary.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 namespace DamageScripts {
     public static class DamageDictionary {
         static DamageDictionary() {
-            damageInformationDictionary = new Dictionary<DamageType, DamageTypeScriptableObject>();
-            damageInformationDictionary.Add(DamageType.SMALL_RIP, Resources.Load<DamageTypeScriptableObject>("ScriptableObjects/PlushieDamage/SmallRip"));
-            damageInformationDictionary.Add(DamageType.LARGE_RIP, Resources.Load<DamageTypeScriptableObject>("ScriptableObjects/PlushieDamage/LargeRip"));
-            damageInformationDictionary.Add(DamageType.LARGE_RIP_STUFFED, Resources.Load<DamageTypeScriptableObject>("ScriptableObjects/PlushieDamage/LargeRipStuffed"));
+            damageInfoDictionary = new Dictionary<DamageType, DamageTypeScriptableObject>();
+            damageInfoDictionary.Add(DamageType.SMALL_RIP, Resources.Load<DamageTypeScriptableObject>("ScriptableObjects/PlushieDamage/SmallRip"));
+            damageInfoDictionary.Add(DamageType.LARGE_RIP, Resources.Load<DamageTypeScriptableObject>("ScriptableObjects/PlushieDamage/LargeRip"));
+            damageInfoDictionary.Add(DamageType.LARGE_RIP_STUFFED, Resources.Load<DamageTypeScriptableObject>("ScriptableObjects/PlushieDamage/LargeRipStuffed"));
         }
 
-        public static Dictionary<DamageType, DamageTypeScriptableObject> damageInformationDictionary;
+        public static Dictionary<DamageType, DamageTypeScriptableObject> damageInfoDictionary;
     }
 }
