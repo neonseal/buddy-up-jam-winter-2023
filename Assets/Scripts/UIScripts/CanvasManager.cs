7883c423e8816681d40b7585fa360bb3857867e4
laumi73 <laumichael73@gmail.com>
2023-02-19 00:35:23 -0500

    Removed unnecessary scripts

--- Assets/Scripts/UIScripts/CanvasManager.cs
+++ Assets/Scripts/UIScripts/CanvasManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Tool;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace GameUI
 {
     public class CanvasManager : MonoBehaviour
     {
         internal GameObject currentTool;
+        private EventSystem eventSystem;
+        private PointerEventData pointerEventData;
+        private GraphicRaycaster raycaster;
 
-        private void Awake()
+        void Start()
         {
             this.currentTool = null;
+            this.raycaster = this.GetComponent<GraphicRaycaster>();
+
+            this.eventSystem = EventSystem.current;
         }
+
+        void Update()
+    {
+        //Check if the left Mouse button is clicked
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            //Set up the new Pointer Event
+            pointerEventData = new PointerEventData(eventSystem);
+            //Set the Pointer Event Position to that of the mouse position
+            pointerEventData.position = Input.mousePosition;
+
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+
+            //Raycast using the Graphics Raycaster and mouse click position
+            raycaster.Raycast(pointerEventData, results);
+
+            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            foreach (RaycastResult result in results)
+            {
+                Debug.Log("Hit " + result.gameObject.name);
+            }
+        }
+    }
     }
 }
 
--- Assets/Scripts/UIScripts/ToolScript.cs
+++ Assets/Scripts/UIScripts/ToolScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using Tool;
 
 namespace GameUI
 {
