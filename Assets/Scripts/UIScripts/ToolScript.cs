a8436b5921fa743c45953703e3712d7dc35a7a41
laumi73 <laumichael73@gmail.com>
2023-02-19 18:39:39 -0500

    Removed unnecessary tool scripts

--- Assets/Scripts/UIScripts/ToolScript.cs
+++ Assets/Scripts/UIScripts/ToolScript.cs
@@ -10,7 +10,7 @@ namespace GameUI
     public class ToolScript : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField]
-        private ToolScriptableObject toolScriptableObject;
+        public readonly ToolScriptableObject toolScriptableObject;
 
         private CanvasManager canvasManager;
         private Image toolImage;
