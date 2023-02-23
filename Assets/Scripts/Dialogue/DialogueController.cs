ee0ec4ab0f1dd1a13dba35dfe56770fdf39067d2
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-02-23 18:33:19 -0500

    Complete basic dialogue system

--- Assets/Scripts/Dialogue/DialogueController.cs
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -1,17 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueController : MonoBehaviour
 {
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+    public Button btn;
+    public Animator animator;
     private Queue<string> sentences;
-    private void Start() {
+
+    private void Start() {       
         sentences = new Queue<string>();
+
         CustomEventManager.current.onTriggerDialogue += StartDialogue;
     }
 
     private void StartDialogue(Dialogue dialogue) {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        StartCoroutine(StartDialogueRoutine(dialogue));
+    }
+
+    public void DisplayNextSentence() {
+        if (sentences.Count <= 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+
+        if (sentences.Count == 0) {
+            btn.gameObject.GetComponentInChildren<TMP_Text>().text = "END";
+        }
+    }
+
+    IEnumerator StartDialogueRoutine(Dialogue dialogue) {
+        // Initialize Dialogue Box for current speaker
+        nameText.text = dialogue.name;
+        dialogueText.text = "";
+
+        animator.SetBool("isOpen", true);
+        // Pause briefly to allow animation to finish before rendering text
+        yield return new WaitForSeconds(.5f);
+
 
         sentences.Clear();
 
@@ -22,17 +56,16 @@ public class DialogueController : MonoBehaviour
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence() {
-        if (sentences.Count <= 0) {
-            EndDialogue();
-            return;
-        }
+    IEnumerator TypeSentence(string sentence) {
+        dialogueText.text = "";
 
-        string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.025f);
+        }
     }
 
     private void EndDialogue() {
-        Debug.Log("END OF CONVERSATION");
+        animator.SetBool("isOpen", false);
     }
 }
