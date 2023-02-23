40dd74c4e306589786801b0f5b3b2a28ed6970b8
laumi73 <laumichael73@gmail.com>
2023-02-23 16:02:47 -0500

    Added ScriptableObjects for plushie and sets of plushies

---
+++ Assets/Scripts/ScriptableObjectScripts/PlushieScriptableObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DamageScripts;
+
+[CreateAssetMenu (fileName = "Plushie", menuName = "Scriptable Objects/Plushie")]
+public class PlushieScriptableObject : ScriptableObject
+{
+    public Sprite plushieSprite;
+    public string backstory;
+    public List<DamageType> damageTypeList;
+    public List<Vector2> damagePositionList;
+}
