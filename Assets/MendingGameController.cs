c3bfc3d72399c86acfbcb6a352601929a7b8a9f2
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-02-27 22:03:55 -0500

    Work on dashed line rendering for sewing game

---
+++ Assets/MendingGameController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MendingGameController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer zoomLense;
+
+    private void Awake() {
+        
+    }
+}
