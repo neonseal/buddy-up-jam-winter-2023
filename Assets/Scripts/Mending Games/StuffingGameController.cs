5178deaf853faf8adf45bb26831cf672669ef6cf
Corey Sieland-Harris <corey.sielandharris@gmail.com>
2023-04-28 12:49:36 -0400

    Add additional brush sizes and start work on multiple target prototype

--- Assets/Scripts/Mending Games/StuffingGameController.cs
+++ Assets/Scripts/Mending Games/StuffingGameController.cs
@@ -25,59 +25,65 @@ public class StuffingGameController : MonoBehaviour
         stuffingMaskTexture = new Texture2D(unstuffedTexBase.width, unstuffedTexBase.height);
         stuffingMaskTexture.SetPixels(unstuffedTexBase.GetPixels());
         stuffingMaskTexture.Apply();
-        material.SetTexture("StuffingMask", stuffingMaskTexture);
-
-
+        material.SetTexture("_Mask", stuffingMaskTexture);
     }
 
-    private void OnMouseDown() {
-        Vector3 position = Input.mousePosition;
-        position.z = 0.0f;
-        position = Camera.main.ScreenToWorldPoint(position);
-        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            Vector3 position = Input.mousePosition;
+            position.z = 0.0f;
+            position = Camera.main.ScreenToWorldPoint(position);
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
 
-        if (hit.collider != null) {
-            // Determine mouse position as percentage of collider extents (4.2, 4.2)
-            float percentX = (4.2f + position.x) / 8.4f;
-            float percentY = (4.2f + position.y) / 8.4f;
+            if (hit.collider != null) {
+                // Determine mouse position as percentage of collider extents 
+                Vector2 targetPosition = this.transform.position;
+                Debug.Log("TARGET POSITION: " + targetPosition);
+                Debug.Log("PARENT POSITION: " + this.transform.parent.transform.position);
+                Debug.Log("MOUSE POSITION: " + position);
 
-            int pixelX = (int)(percentX * stuffingMaskTexture.width);
-            int pixelY = (int)(percentY * stuffingMaskTexture.height);
+                float percentX = position.x < 0 ? (position.x + 2.8f) / 2.8f : position.x / 2.8f;
+                float percentY = position.y < 0 ? (position.y + 2.8f) / 2.8f : position.y / 2.8f;
 
-            Vector2Int paintPixelPosition = new Vector2Int(pixelX, pixelY);
-
-            int paintPixelDistance = Mathf.Abs(paintPixelPosition.x - lastPaintPixelPosition.x) + Mathf.Abs(paintPixelPosition.y - lastPaintPixelPosition.y);
-            int maxPaintDistance = 7;
-            if (paintPixelDistance < maxPaintDistance) {
-                // Painting too close to last position
-                return;
-            }
-            lastPaintPixelPosition = paintPixelPosition;
-
-            int pixelXOffset = pixelX - (stuffingBrush.width / 2);
-            int pixelYOffset = pixelY - (stuffingBrush.height / 2); 
-            // Debug.Log("Width: " + stuffingBrush.width + " | Height: " + stuffingBrush.height);
+                int pixelX = (int)(percentX * 300);
+                int pixelY = (int)(percentY * 300);
+                Debug.Log("X: " + percentX + " | Y: " + percentY);
 
 
-            for (int x = 0; x < stuffingBrush.width; x++) {
-                for (int y = 0; y < stuffingBrush.height; y++) {
-                    Color pixelDirt = stuffingBrush.GetPixel(x, y);
-                    Color pixelDirtMask = stuffingMaskTexture.GetPixel(pixelXOffset + x, pixelYOffset + y);
-                    Debug.Log(pixelDirtMask);
+                Vector2Int paintPixelPosition = new Vector2Int(pixelX, pixelY);
 
-                    float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
-                    dirtAmount -= removedAmount;
-
-                    stuffingMaskTexture.SetPixel(
-                        pixelXOffset + x,
-                        pixelYOffset + y,
-                        new Color(0, pixelDirtMask.g * pixelDirt.g, 0)
-                    );
+                int paintPixelDistance = Mathf.Abs(paintPixelPosition.x - lastPaintPixelPosition.x) + Mathf.Abs(paintPixelPosition.y - lastPaintPixelPosition.y);
+                int maxPaintDistance = 7;
+                if (paintPixelDistance < maxPaintDistance) {
+                    // Painting too close to last position
+                    return;
                 }
+                lastPaintPixelPosition = paintPixelPosition;
+
+                int pixelXOffset = pixelX - (stuffingBrush.width / 2);
+                int pixelYOffset = pixelY - (stuffingBrush.height / 2);
+
+
+                for (int x = 0; x < stuffingBrush.width; x++) {
+                    for (int y = 0; y < stuffingBrush.height; y++) {
+                        Color pixelDirt = stuffingBrush.GetPixel(x, y);
+                        Color pixelDirtMask = stuffingMaskTexture.GetPixel(pixelXOffset + x, pixelYOffset + y);
+                        //Debug.Log(pixelDirtMask);
+
+                        float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
+                        dirtAmount -= removedAmount;
+
+                        stuffingMaskTexture.SetPixel(
+                            pixelXOffset + x,
+                            pixelYOffset + y,
+                            new Color(0, pixelDirtMask.g * pixelDirt.g, 0)
+                        );
+                    }
+                }
+
+
+                stuffingMaskTexture.Apply();
             }
-
-
-            stuffingMaskTexture.Apply();
         }
     }
 }
