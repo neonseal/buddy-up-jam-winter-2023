b4719bd1d225dc783933c99b1ea059964ecbca3b
laumi73 <laumichael73@gmail.com>
2023-05-03 12:58:26 -0400

    Added bell-related components and objects

---
+++ Assets/Scripts/GameUIElements/Bell.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bell : MonoBehaviour
+{
+    private Button _bellButton;
+
+    private void Awake()
+    {
+        this._bellButton = GetComponentInChildren<Button>();
+    }
+
+    private void Start()
+    {
+        this._bellButton.onClick.AddListener(this.bellClick);
+    }
+
+    private void bellClick()
+    {
+        Debug.Log("Ding!");
+    }
+}
