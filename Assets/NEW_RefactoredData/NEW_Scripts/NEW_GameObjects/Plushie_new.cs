87f911dea27ff68b33526af1322ee5ddcc00785c
laumi73 <laumichael73@gmail.com>
2023-06-03 15:53:00 -0400

    Added basic structure for new plushie and plushieDamage Script

---
+++ Assets/NEW_RefactoredData/NEW_Scripts/NEW_GameObjects/Plushie_new.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plushie_new : MonoBehaviour
+{
+    public String customerRequestDialogue;
+    public ClientCard clientCard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._spawnPlushie();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void _spawnPlushie() {
+
+    }
+}
