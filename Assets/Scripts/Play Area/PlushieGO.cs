7e35f73668c5a409648bbc26977f34a8f3c5653a
laumi73 <laumichael73@gmail.com>
2023-07-12 22:54:32 -0400

    Replaced PlushieGO with things from the new version

--- Assets/Scripts/Play Area/PlushieGO.cs
+++ Assets/Scripts/Play Area/PlushieGO.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlushieGO : MonoBehaviour {
-    private PlushieDamageGO[] damageList;
-    private PlushieDamageSM damageStateMachine;
+     // The sprite that is applied to plushie when repair is completed
+    public Sprite repairedPlushieSprite;
 
-    private void Awake() {
-        damageList = this.GetComponentsInChildren<PlushieDamageGO>();
+    public GameObject customerRequestDialogue;
+    //public ClientCard clientCard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.spawnPlushie();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnPlushie() {
 
     }
 }
